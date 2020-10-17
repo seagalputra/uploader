32a1679dacda8eb87f24a5a18d2177d2cc93e9e8
seagalputra <seagalputra@gmail.com>
2020-10-17 21:26:17 +0700

    Adding upload placeholder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,6 @@ require("channels")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../../assets/images', true)
+const imagePath = (name) => images(name, true)
 import 'stylesheets/application'
