777f96e1a0948aa965b8dbcc03ea7b05af580f43
seagalputra <seagalputra@gmail.com>
2020-10-24 14:24:47 +0700

    Change unit test with headless_chrome

--- test/controllers/buckets_controller_test.rb
+++ test/controllers/buckets_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class BucketsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get buckets_index_url
+    get root_url
     assert_response :success
   end
 
